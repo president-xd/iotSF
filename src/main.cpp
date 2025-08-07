1c0ac849a9127219f0379176ce93fdfc449512d1
president-xd <lasharimohsin19@gmail.com>
2025-08-07 10:07:07 -0400

    Hello iotSF

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include <sstream>
+#include <iterator>
+
+int main() {
+    std::cout << "Hello by iotSF developers" << std::endl;
+}
