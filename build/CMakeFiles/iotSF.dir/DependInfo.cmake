34b1191c5df10e1f6100eaa7dc1d1c70c0966953
president-xd <lasharimohsin19@gmail.com>
2025-08-08 05:58:27 -0700

    SUCCESSFULL_BUILD

--- build/CMakeFiles/iotSF.dir/DependInfo.cmake
+++ build/CMakeFiles/iotSF.dir/DependInfo.cmake
@@ -8,8 +8,8 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/kali/iotSF/src/main.cpp" "CMakeFiles/iotSF.dir/src/main.cpp.o" "gcc" "CMakeFiles/iotSF.dir/src/main.cpp.o.d"
-  "" "iotSF" "gcc" "CMakeFiles/iotSF.dir/link.d"
+  "/home/president/iotSF/src/main.cpp" "CMakeFiles/iotSF.dir/src/main.cpp.o" "gcc" "CMakeFiles/iotSF.dir/src/main.cpp.o.d"
+  "/home/president/iotSF/src/wifi/usb/usb_detector.cpp" "CMakeFiles/iotSF.dir/src/wifi/usb/usb_detector.cpp.o" "gcc" "CMakeFiles/iotSF.dir/src/wifi/usb/usb_detector.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- build/CMakeFiles/iotSF.dir/cmake_clean.cmake
+++ build/CMakeFiles/iotSF.dir/cmake_clean.cmake
@@ -1,7 +1,8 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/iotSF.dir/link.d"
   "CMakeFiles/iotSF.dir/src/main.cpp.o"
   "CMakeFiles/iotSF.dir/src/main.cpp.o.d"
+  "CMakeFiles/iotSF.dir/src/wifi/usb/usb_detector.cpp.o"
+  "CMakeFiles/iotSF.dir/src/wifi/usb/usb_detector.cpp.o.d"
   "iotSF"
   "iotSF.pdb"
 )
